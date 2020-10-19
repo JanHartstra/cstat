88d0d32170d62a32425c452a952a57199e19e946
JanHartstra <jan.hartstra@gmail.com>
2020-10-20 01:01:23 +0200

    Vector structure added.

--- dscstat.c
+++ dscstat.c
@@ -7,6 +7,34 @@ Date:    2020-10-19
 
 #include <stdio.h>
 
+#define MAX_VECTOR_SIZE 100
+
+typedef struct vector {
+   double data[MAX_VECTOR_SIZE];
+   int n;
+} vector;
+
+vector test_vector(void) 
+{
+   vector test;
+   test.n = 5;
+   test.data[0] = 11;
+   test.data[1] = 12;
+   test.data[2] = 13;
+   test.data[3] = 14;
+   test.data[4] = 15;   
+   return test;
+}
+
+void print_vector (vector v)
+{
+   int i;
+   for (i = 0; i < v.n; i++)
+   {
+      printf("Element %d : %9.3f\n", i, v.data[i]);
+   }
+}
+
 int main(int argc, char *argv[])
 {
    int i;
@@ -25,6 +53,9 @@ int main(int argc, char *argv[])
        printf("argument list is empty.\n");
        /* Allow the user to enter data */ 
    }
+   vector v;
+   v = test_vector();
+   print_vector(v);
    /* Calculate descriptive statistics for the data in data vector */
    /* Print the descriptive statistics to standard out (or file?) */
    return 0;
